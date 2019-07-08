21c0cb6f46249569b1f874e4edcae72fd1db8eeb
GrapeColor <grapecolor@GrapeColor.local>
2019-07-08 19:18:43 +0900

    fix routes and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:index, :show, :edit]
   resources :books, except: [:new]
-  get 'index' => 'home#index'
+  get '/' => 'home#index'
   get 'home/about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
