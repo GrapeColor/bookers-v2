064ad26c72ae5d2e91cba9f6ae403eec1eab882f
GrapeColor <grapecolor@GrapeColor.local>
2019-07-09 15:47:29 +0900

    edit common leyout and index view

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  # get 'users/index'
-  # get 'users/show'
+  root 'home#index'
   devise_for :users
   resources :users, only: [:index, :show, :edit]
   resources :books, except: [:new]
-  get '/' => 'home#index'
   get 'home/about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
