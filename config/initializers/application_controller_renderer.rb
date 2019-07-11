dc5f3f18f42e7c82c28ae732fe15ba86152f17c3
GrapeColor <grapecolor@GrapeColor.local>
2019-07-11 14:57:46 +0900

    fix rspec failures

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '39fccde9a097492fdf0d9f403ce00b52b67711d0d17b714e2055919f817cae23b8d3d797a6b33de0bbea9b03ee6beb9f3a8ebe0e4f0bb739708e230ee1cc059b'
